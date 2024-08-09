7893f112b7ea7820abb90672e7bbc89d232ed67d
Reetika888 <reetikauths@gmail.com>
2024-08-09 11:40:22 +0530

    top rated feature completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
 import RestauarantCard from "./Restauarant";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState , useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-const [ListOfRestaurants,setListOfRestauarants] = useState(resList);
-    return (
+const [listOfRestaurants,setListOfRestauarants] = useState([]);
+
+useEffect(()=>{
+  console.log("use effect called");
+  fetchData();
+},[]);
+
+const fetchData = async() => {
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const json = await data.json();
+  // optional chaining
+  setListOfRestauarants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+}
+
+    return listOfRestaurants === 0 ? <Shimmer /> :  (
       <div className="body">
           <div className="filter">
               <button className="filter-btn" onClick={()=>{
@@ -17,7 +30,7 @@ const [ListOfRestaurants,setListOfRestauarants] = useState(resList);
           </div>
           <div className="res-container">
           {
-              ListOfRestaurants.map((restaurant)=> (<RestauarantCard key={restaurant.info.id} resData = {restaurant} />)
+              listOfRestaurants.map((restaurant)=> (<RestauarantCard key={restaurant.info.id} resData = {restaurant} />)
           )
           }
           </div>
